4b6618f8325838ebbfde74d2ff0bc9a3fd7c8ef1
김준형 <ghilbut@gmail.com>
2023-02-10 02:03:40 +0900

    Make multi-stage dockerfile build

--- k8single/k8single/settings.py
+++ k8single/k8single/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,3 +121,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = []
+
+print(f'STATIC_ROOT: {STATIC_ROOT}')
--- k8single/k8single/urls.py
+++ k8single/k8single/urls.py
@@ -19,3 +19,8 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+from django.conf import settings
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
