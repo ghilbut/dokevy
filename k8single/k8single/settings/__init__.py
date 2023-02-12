5406cfe3c402df220fec2d434d3d80b5e8805b88
김준형 <ghilbut@gmail.com>
2023-02-12 16:39:22 +0900

    Change backend to mariadb/mysql from sqlite3

--- k8single/k8single/settings/__init__.py
+++ k8single/k8single/settings/__init__.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'k8single.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {},
+        'NAME': 'k8single',
+        'USER': 'k8single',
+        'PASSWORD': 'k8single',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
